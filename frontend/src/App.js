181362641c9abdddaa645d6904386016059e3c75
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2021-10-24 12:53:08 -0500

    Agregando el Routing

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Pacientes from './components/Pacientes';
+import NuevaCita from './components/NuevaCita';
+import Cita from './components/Cita';
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route 
+          exact path="/" 
+          component={Pacientes} 
+        />
+				<Route 
+          exact path="/nueva" 
+          component={NuevaCita} 
+        />
+        <Route 
+          exact path="/cita/:id" 
+          component={Cita} 
+        />
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
