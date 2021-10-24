1bb3379649eaf6ba878291e978b51e4e5e72c4fc
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2021-10-24 14:38:31 -0500

    Agregando la vista de citas en la página principal

--- API/index.js
+++ API/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 //Crear el servidor
 const servidor = express();
+
+//Usando Cors para consulta desde una url definida
+const whiteList = ['http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) =>{
+        // console.log(origin);
+        const existe = whiteList.some( dominio => dominio === origin);
+        if (existe) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
+//Habilitar Cors
+// servidor.use( cors(corsOptions) );
+servidor.use(cors());
 
 //Conectar a mongoDB
 mongoose.Promise = global.Promise;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import clienteAxios from './config/axios';
 
 import Pacientes from './components/Pacientes';
 import NuevaCita from './components/NuevaCita';
 import Cita from './components/Cita';
 
 function App() {
+  //State de la app
+  const [citas, guardarCitas] = useState([]);
+  useEffect( () => {
+    const consultarAPI = () =>{
+      clienteAxios.get('/pacientes')
+        .then( respuesta => {
+          //Colocar en el state el resultado
+          guardarCitas(respuesta.data);
+        })
+        .catch( error => {
+          console.log(error);
+        });
+    }
+    consultarAPI();
+  }, []);
+
 	return (
 		<Router>
 			<Switch>
 				<Route 
           exact path="/" 
-          component={Pacientes} 
+          component={() => <Pacientes 
+            citas={citas}/>} 
         />
 				<Route 
           exact path="/nueva" 
